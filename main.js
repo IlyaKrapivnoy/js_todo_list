3dc059ebce69f4e2626d305f54e9e201980d93f7
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-06-29 12:32:20 +0300

    add checked state on click

--- main.js
+++ main.js
@@ -14,5 +14,12 @@ for (let i = 0; i < close.length; i++) {
     close[i].onclick = function() {
         var div = this.parentElement;
         div.style.display = 'none';
-    }
+    } 
 }
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+    if(ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+}, false);
