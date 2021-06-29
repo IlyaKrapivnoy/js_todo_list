ce0b786c051fc11e961cc868a4142a8aff6a81bb
ilya.krapivnoy <ilya.krapivnoy@gmail.com>
2021-06-29 12:28:34 +0300

    add delete func on click

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-let myNodeList = document.getElementsByTagName('LI');
-let i;
-for (let i = 0; i < myNodeList.length; i++) {
-    let span = document.createElement('SPAN');
-    let txt = document.createTextNode('\u00D7');
+var myNodeList = document.getElementsByTagName('LI');
+var i;
+for (var i = 0; i < myNodeList.length; i++) {
+    var span = document.createElement('SPAN');
+    var txt = document.createTextNode('\u00D7');
     span.className = 'close';
     span.appendChild(txt);
     myNodeList[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName('close');
+var i;
+for (let i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = 'none';
+    }
 }
